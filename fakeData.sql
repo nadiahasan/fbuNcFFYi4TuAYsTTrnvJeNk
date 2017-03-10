c9cd73828add1d94fa8190041c3f7438e48d25cf
nadiahasan <nadiahasan2015@gmail.com>
2017-03-10 09:31:01 -0500

    Fake data
    I populated fake users and 5 courses.

---
+++ fakeData.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO USERS VALUES('admin', 'pass' ,'nadiahasan2015@gmail.com', 'Nadia', 'Hasan', 1);
+INSERT INTO USERS VALUES('user1', '1234' , 'haroldwelch@gmail.com','Harold', 'Welch', 2);
+INSERT INTO USERS VALUES('user2', '1234' ,'thomasjohnson@gmail.com', 'Thomas', 'Johnson', 2);
+INSERT INTO USERS VALUES('user3', '1234' , 'jamesallmon@gmail.com','James', 'Allmon', 2);
+INSERT INTO USERS VALUES('user4', '1234' , 'annrochester@gmail.com','Ann', 'Rochester', 2);
+
+INSERT INTO COURSE VALUES('CEN4010', 'Software Engineering', 'IT', 'Software life cycle' );
+INSERT INTO COURSE VALUES('CNT3004', 'Computer Networks', 'IT', 'Introduction to computer networks' );
+INSERT INTO COURSE VALUES('COP4610', 'Operating Systems', 'IT', 'Concepts of operating systems' );
+INSERT INTO COURSE VALUES('COP3530', 'Data Structures', 'IT', 'Introduction to algorithms and data structures' );
+INSERT INTO COURSE VALUES('COP3834', 'Web App Development', 'IT', 'Introduction to web app development' );
