803ffefc876dabbe3d762765b71b629cb7445d01
nadiahasan <nadiahasan2015@gmail.com>
2017-03-10 10:13:52 -0500

    Modified Fake Data
    Passwords are hashed now for security purposes.

--- fakeData.sql
+++ fakeData.sql
@@ -1,9 +1,9 @@
 
-INSERT INTO USERS VALUES('admin', 'pass' ,'nadiahasan2015@gmail.com', 'Nadia', 'Hasan', 1);
-INSERT INTO USERS VALUES('user1', '1234' , 'haroldwelch@gmail.com','Harold', 'Welch', 2);
-INSERT INTO USERS VALUES('user2', '1234' ,'thomasjohnson@gmail.com', 'Thomas', 'Johnson', 2);
-INSERT INTO USERS VALUES('user3', '1234' , 'jamesallmon@gmail.com','James', 'Allmon', 2);
-INSERT INTO USERS VALUES('user4', '1234' , 'annrochester@gmail.com','Ann', 'Rochester', 2);
+INSERT INTO USERS VALUES('admin', '$2y$10$I/U7I.gPIlLVzj9mDAdCx.iyBI2.a32GyfV.zbBSESntjQTS87hvO' ,'nadiahasan2015@gmail.com', 'Nadia', 'Hasan', 1);
+INSERT INTO USERS VALUES('user1', '$2y$10$RUaecKIyoIMDY3TsnzJqZe7e/SXFnasM.sHUIdFEIe76KLVqBa.72' , 'haroldwelch@gmail.com','Harold', 'Welch', 2);
+INSERT INTO USERS VALUES('user2', '$2y$10$RUaecKIyoIMDY3TsnzJqZe7e/SXFnasM.sHUIdFEIe76KLVqBa.72' ,'thomasjohnson@gmail.com', 'Thomas', 'Johnson', 2);
+INSERT INTO USERS VALUES('user3', '$2y$10$RUaecKIyoIMDY3TsnzJqZe7e/SXFnasM.sHUIdFEIe76KLVqBa.72' , 'jamesallmon@gmail.com','James', 'Allmon', 2);
+INSERT INTO USERS VALUES('user4', '$2y$10$RUaecKIyoIMDY3TsnzJqZe7e/SXFnasM.sHUIdFEIe76KLVqBa.72' , 'annrochester@gmail.com','Ann', 'Rochester', 2);
 
 INSERT INTO COURSE VALUES('CEN4010', 'Software Engineering', 'IT', 'Software life cycle' );
 INSERT INTO COURSE VALUES('CNT3004', 'Computer Networks', 'IT', 'Introduction to computer networks' );
